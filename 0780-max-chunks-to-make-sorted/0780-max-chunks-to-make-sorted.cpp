26e3d3505782ec9966207631a93a64cc42dc0f36
kovidH333 <126075570+kovidH333@users.noreply.github.com>
2024-12-20 11:13:00 +0530

    Time: 0 ms (100%), Space: 9.3 MB (7.22%) - LeetHub

--- 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
@@ -1,23 +1,40 @@
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
-        int n = arr.size();
-        // Stack to store the maximum elements of each chunk
-        stack<int> monotonicStack;
-        for (int i = 0; i < n; i++) {
-            // Case 1: Current element is larger, starts a new chunk
-            if (monotonicStack.empty() || arr[i] > monotonicStack.top()) {
-                monotonicStack.push(arr[i]);
-            } else {
-                // Case 2: Merge chunks
-                int maxElement = monotonicStack.top();
-                while (!monotonicStack.empty() &&
-                       arr[i] < monotonicStack.top()) {
-                    monotonicStack.pop();
-                }
-                monotonicStack.push(maxElement);
+        int cnt=0;
+        int maxi = 0;
+        for(int i=0;i<arr.size();i++){
+            maxi = max(maxi , arr[i]);
+            if(maxi==i){
+                cnt++;
             }
         }
-        return monotonicStack.size();
+        return cnt;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int maxChunksToSorted(vector<int>& arr) {
+//         int n = arr.size();
+//         // Stack to store the maximum elements of each chunk
+//         stack<int> monotonicStack;
+//         for (int i = 0; i < n; i++) {
+//             // Case 1: Current element is larger, starts a new chunk
+//             if (monotonicStack.empty() || arr[i] > monotonicStack.top()) {
+//                 monotonicStack.push(arr[i]);
+//             } else {
+//                 // Case 2: Merge chunks
+//                 int maxElement = monotonicStack.top();
+//                 while (!monotonicStack.empty() &&
+//                        arr[i] < monotonicStack.top()) {
+//                     monotonicStack.pop();
+//                 }
+//                 monotonicStack.push(maxElement);
+//             }
+//         }
+//         return monotonicStack.size();
+//     }
+// };
