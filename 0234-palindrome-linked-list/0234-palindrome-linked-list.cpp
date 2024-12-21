59083138bd8d3c7b054617e8c40c9d7be94b0546
kovidH333 <126075570+kovidH333@users.noreply.github.com>
2024-12-21 23:03:38 +0530

    Time: 4 ms (53.53%), Space: 117.9 MB (70.64%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -8,22 +8,55 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     bool isPalindrome(ListNode* head) {
+//         vector<int> arr;
+//         ListNode* temp = head;
+//         while(temp){
+//             arr.push_back(temp->val);
+//             temp = temp->next;
+//         }
+//         int n = arr.size();
+//         int i = 0;
+//         int j = n-1;
+//         while(i<=j){
+//             if(arr[i]!=arr[j]) return false;
+//             i++;
+//             j--;
+//         }
+//         return true;
+//     }
+// };
+
+
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        vector<int> arr;
-        ListNode* temp = head;
-        while(temp){
-            arr.push_back(temp->val);
-            temp = temp->next;
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        int n = arr.size();
-        int i = 0;
-        int j = n-1;
-        while(i<=j){
-            if(arr[i]!=arr[j]) return false;
-            i++;
-            j--;
+
+        ListNode* temp = slow;
+        ListNode* prev = nullptr;
+        ListNode* front = nullptr;
+        while(temp){
+            front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        }
+
+        ListNode* temp1 = head;
+        ListNode* temp2 = prev;
+        while(temp2){
+            if(temp1->val != temp2->val) return false;
+            temp1 = temp1->next;
+            temp2 = temp2->next;
         }
         return true;
     }
