7c1de46c372bd5fd40a5c9f7d573e8a697f7c91e
kovidH333 <126075570+kovidH333@users.noreply.github.com>
2024-12-27 23:08:54 +0530

    Time: 8 ms (64.76%), Space: 11.7 MB (72.28%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,20 +1,43 @@
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         map<pair<char,int>> unq;
+//         int n = s.size();
+//         int l=0,r=0,maxlen=0;
+
+//         while(r<n){
+//             if(unq.find(s[r]) == unq.end()){
+//                 unq.insert({s[r],r});
+//                 maxlen = max(maxlen, r-l+1);
+//                 r++;
+//             }else{
+//                 unq.erase(s[l]);
+//                 l = unq
+//             }
+//         }
+//         return maxlen;
+//     }
+// };
+
+
+#include <unordered_map>
+#include <string>
+#include <algorithm>
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        set<char> unq;
-        int n = s.size();
-        int l=0,r=0,maxlen=0;
+        unordered_map<char, int> charMap;
+        int left = 0, maxLength = 0;
 
-        while(r<n){
-            if(unq.find(s[r]) == unq.end()){
-                unq.insert(s[r]);
-                maxlen = max(maxlen, r-l+1);
-                r++;
-            }else{
-                unq.erase(s[l]);
-                l++;
+        for (int right = 0; right < s.size(); right++) {
+            if (charMap.find(s[right]) != charMap.end() && charMap[s[right]] >= left) {
+                left = charMap[s[right]] + 1; 
             }
+            charMap[s[right]] = right; 
+            maxLength = max(maxLength, right - left + 1); 
         }
-        return maxlen;
+
+        return maxLength;
     }
 };
