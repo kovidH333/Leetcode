12266005e35db8732cef472dc8989202486271e7
kovidH333 <126075570+kovidH333@users.noreply.github.com>
2024-12-28 21:19:12 +0530

    Time: 15 ms (41.02%), Space: 11 MB (45.67%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,24 +1,16 @@
+// 
+
 class Solution {
 public:
     int numberOfSubstrings(string s) {
         int l = 0;
-        int n = s.size();
-        int hash[3] = {0};
-        int typecnt = 0;
-        long long cnt = 0;
-
-        for(int r=0; r<n; r++){
-            if(hash[s[r]-'a'] == 0) typecnt++;
-            hash[s[r]-'a']++;
-
-            while(typecnt>2){
-                hash[s[l]-'a']--;
-                if(hash[s[l]-'a'] == 0) typecnt--;
-                l++;
-            }
-
-            cnt += r-l+1;
+        int hash[3] = {-1,-1,-1};
+        int cnt = 0;
+        
+        for(int r=0; r<s.size(); r++){
+            hash[s[r]-'a'] = r;
+            cnt += min({hash[0],hash[1],hash[2]})+1;
         }
-        return static_cast<long long>(n) * (n + 1) / 2 - cnt;
+        return cnt;
     }
 };
