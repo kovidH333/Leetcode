89587a0e3d375b41147cfa3a85af2bb6833a3772
kovidH333 <126075570+kovidH333@users.noreply.github.com>
2025-01-06 12:25:57 +0530

    Time: 15 ms (96.29%), Space: 9.7 MB (90.15%) - LeetHub

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minInsertions(string s) {
+        string s1 = s;
+        reverse(s1.begin(),s1.end());
+        int n = s.size();
+        vector<int> prev(n+1,0),curr(n+1,0);
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(s[i-1]==s1[j-1]) curr[j] = 1 + prev[j-1];
+                else curr[j] = max(prev[j],curr[j-1]);
+            }
+            prev = curr;
+        }
+        int cnt = prev[n];
+        if(cnt==0) return n-1;
+        else return n-cnt;
+    }
+};
