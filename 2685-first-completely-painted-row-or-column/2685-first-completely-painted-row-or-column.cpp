7d52079df9a5da26db75d4d0f9adc95bde55f9a2
kovidH333 <126075570+kovidH333@users.noreply.github.com>
2025-01-23 15:47:11 +0530

    Time: 85 ms (73.48%), Space: 161.7 MB (67.45%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
@@ -1,26 +1,68 @@
+// class Solution {
+// public:
+//     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+//         int m = mat.size();
+//         int n = mat[0].size();
+
+//         vector<int> row(m,0);
+//         vector<int> col(n,0);
+
+//         unordered_map<int, pair<int, int>> posMap;
+//         for (int i = 0; i < m; ++i) {
+//             for (int j = 0; j < n; ++j) {
+//                 posMap[mat[i][j]] = {i, j}; 
+//             }
+//         }
+
+//         for(int t=0; t<arr.size(); t++){
+//             row[posMap[arr[t]].first]++;
+//             col[posMap[arr[t]].second]++;
+//             if(row[posMap[arr[t]].first]==n||col[posMap[arr[t]].second]==m){
+//                 return t;
+//             }
+//         }
+//         return m*n-max(m,n);
+//     }
+// };
+
+
+//Mind blowing solution
+// Instead of checking each row and column after every iteration, we will check for all rows and columns, at what index in the array arr, they will be painted completely, i.e., the element having largest index in array arr. Smallest of those will be the required answer.
+
 class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
-        int m = mat.size();
-        int n = mat[0].size();
+        int numRows = mat.size();
+        int numCols = mat[0].size();
 
-        vector<int> row(m,0);
-        vector<int> col(n,0);
-
-        unordered_map<int, pair<int, int>> posMap;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                posMap[mat[i][j]] = {i, j}; 
-            }
+        unordered_map<int, int> numToIndex;
+        for (int i = 0; i < arr.size(); i++) {
+            numToIndex[arr[i]] = i;
         }
 
-        for(int t=0; t<arr.size(); t++){
-            row[posMap[arr[t]].first]++;
-            col[posMap[arr[t]].second]++;
-            if(row[posMap[arr[t]].first]==n||col[posMap[arr[t]].second]==m){
-                return t;
+        int result = INT_MAX;
+
+        for (int row = 0; row < numRows; row++) {
+            int lastElementIndex = INT_MIN;
+            for (int col = 0; col < numCols; col++) {
+                int indexVal = numToIndex[mat[row][col]];
+                lastElementIndex = max(lastElementIndex, indexVal);
             }
+            result = min(result, lastElementIndex);
         }
-        return m*n-max(m,n);
+
+        for (int col = 0; col < numCols; col++) {
+            int lastElementIndex = INT_MIN;
+            for (int row = 0; row < numRows; row++) {
+                int indexVal = numToIndex[mat[row][col]];
+                lastElementIndex = max(lastElementIndex, indexVal);
+            }
+            result = min(result, lastElementIndex);
+        }
+
+        return result;
+
+
     }
 };
+
